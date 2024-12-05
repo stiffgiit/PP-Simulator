8e32898ef3d5908a654526047a8ba9a3f5020775
stiffgiit <233856@student.krakow.uek.pl>
2024-12-05 21:17:05 +0100

    Stwory

---
+++ Simulator/Creature.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simulator;
+
+public class Creature
+{
+    public string Name { get; }
+    public int Level { get; }
+
+    public Creature(string name, int level = 1)
+    {
+        Name = name;
+        Level = level;
+    }
+
+    
+    public Creature()
+    {
+    }
+
+    public void SayHi() => Console.WriteLine($"Hi, I'm {Name}, my level is {Level}.");
+    
+
+    public string Info => $"{Name} <{Level}>";
+}
--- Simulator/Program.cs
+++ Simulator/Program.cs
@@ -5,5 +5,15 @@ internal class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Starting Simulator!\n");
+
+        var creature = new Creature("Orc",7);
+        creature.SayHi();
+        Console.WriteLine(creature.Info);
+
+
+        var animal = new Animals { Description = "Dogs", Size = 3 };
+        Console.WriteLine(animal.Info);
+
+
     }
 }
