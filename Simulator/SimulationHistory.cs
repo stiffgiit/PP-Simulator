3214b208a669c9ca937a8cc33b8011ad608ff0d7
stiffgiit <233856@student.krakow.uek.pl>
2025-01-16 20:11:07 +0100

    Historia-2

---
+++ Simulator/SimulationHistory.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simulator;
+
+public class SimulationHistory
+{
+    private Simulation _simulation { get; }
+    public int SizeX { get; }
+    public int SizeY { get; }
+    public List<SimulationTurnLog> TurnLogs { get; } = [];
+    // store starting positions at index 0
+
+    public SimulationHistory(Simulation simulation)
+    {
+        _simulation = simulation ??
+            throw new ArgumentNullException(nameof(simulation));
+        SizeX = _simulation.Map.SizeX;
+        SizeY = _simulation.Map.SizeY;
+        Run();
+    }
+
+    private void Run()
+    {
+        // implement
+    }
+}
